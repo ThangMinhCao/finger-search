e7470ad505c2005d3217fb1c6a548265957ba7c6
ThangMinhCao <thangminhcao@gmail.com>
2021-01-23 10:26:32 -0500

    Add .idea to gitignore

---
+++ include/Treap.h
@@ -0,0 +1,14 @@
+//
+// Created by hswin on 2021-01-23.
+//
+
+#ifndef FINGER_SEARCH_TREAP_H
+#define FINGER_SEARCH_TREAP_H
+
+
+class Treap {
+
+};
+
+
+#endif //FINGER_SEARCH_TREAP_H
---
+++ src/Treap.cpp
@@ -0,0 +1,5 @@
+//
+// Created by hswin on 2021-01-23.
+//
+
+#include "../include/Treap.h"
